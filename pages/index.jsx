98ba669dfc351cf527a90645f687f1cccdace917
LuteJr <101115777+LuteJr@users.noreply.github.com>
2022-06-20 21:51:53 +0300

    2151

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,22 +11,6 @@ export default function Home() {
       <Header></Header>
 
       <main className="content"></main>
-      <div className="badge">20off</div>
-      <div className="badge badge-secondary">new entry</div>
-      <div className="badge badge-oversized">%</div>
-
-      <div>
-        <a href="/" className="button">
-          Test button
-        </a>
-      </div>
-
-      <div>
-        <a href="/" className="button button-large">
-          Test button
-        </a>
-      </div>
-
       <footer className="footer"></footer>
     </div>
   );
